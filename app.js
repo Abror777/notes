11f9d348b3979d2cca7b1a63f9eb671f32b5a56f
Abror Olimov <00008996@wiut.uz>
2021-03-12 17:56:49 +0500

    add some new pages

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require('express')
 const app  = express()      
 
+const notes = [
+    'Some awesome note 1',
+    'Some awesome note 2',
+    'Some awesome note 3'
+];
+
 app.use('/static', express.static('public'))
 
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
 	res.render('home')
+})
+
+app.get('/create', (req, res) => {
+	res.render('create')
+})
+
+app.get('/notes', (req, res) => {
+	res.render('notes', { n: notes })
+})
+
+app.get('/notes/1', (req, res) => {
+	res.render('detail')
 })
 
 app.listen(8000, err => {
